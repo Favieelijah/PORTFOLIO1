3c3e1dfc81edd8ce1df8a5a01bba89d5e2f57f65
Favieelijah <favieelijah0@gmail.com>
2025-06-25 18:57:22 +0300

    Initial commit

---
+++ port.jsx
@@ -0,0 +1,14 @@
+const projects=[]
+function openPage() {
+  window.location.href = "https://github.com/Favieelijah/portfolio"; }
+document.getElementById("projectsbtn").addEventListener("click", openPage);
+
+function pageopen(){
+  window.location.href = "https://docs.google.com/document/d/1NHgOVORElxNVq6W1VGHdX6OS-nC3OUi79Kem2JtwpGs/edit?tab=t.0#heading=h.ca0awj8022e2";
+}
+document.getElementById("skillsbtn").addEventListener("click", pageopen);
+
+function contactopen(){
+
+}
+document.getElementById("contactbtn").addEventListener("click", contactopen);
